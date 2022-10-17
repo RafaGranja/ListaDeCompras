9e30ff89b3bfb664d392328753d28dbd1de9626c
Rafael Granja <rafaelgranja2003@gmail.com>
2022-10-16 22:37:31 -0300

    Consertando merda

--- ProjetoBase/app/src/main/java/com/example/projetobase/FormLogin.java
+++ ProjetoBase/app/src/main/java/com/example/projetobase/FormLogin.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 public class FormLogin extends AppCompatActivity {
@@ -15,10 +16,24 @@ public class FormLogin extends AppCompatActivity {
         getSupportActionBar().hide();
 
     }
-    protected void LoadLogin(){
-
+    /** Called when the user touches the button */
+    public void LoadLogin(View view) {
         ProgressBar load= (ProgressBar)findViewById(R.id.progress_bar);
         load.setVisibility(View.VISIBLE);
 
+        EditText login = (EditText)findViewById((R.id.edit_text_login));
+        EditText password = (EditText)findViewById((R.id.edit_text_password));
+
+        if(login.getText().length()==0){
+            login.setHintTextColor(getResources().getColor(R.color.red));
+        }
+        else if(password.getText().length()==0){
+            login.setHintTextColor(getResources().getColor(R.color.black));
+            password.setHintTextColor(getResources().getColor(R.color.red));
+        }
+        else{
+            password.setHintTextColor(getResources().getColor(R.color.black));
+            load.setVisibility(View.INVISIBLE);
+        }
     }
 }
